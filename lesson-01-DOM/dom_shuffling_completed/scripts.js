1f7e5f254915bee223c7e5a4722a9fc2f4cfe8c0
Miles Abbason <miles.abbason@gmail.com>
2022-01-13 16:29:42 -0500

    Working through DOM mixup assignment

---
+++ lesson-01-DOM/dom_shuffling_completed/scripts.js
@@ -0,0 +1,4 @@
+let heading = document.querySelector('h1');
+let navBar = document.body.getElementsByTagName('nav');
+navBar.insertBefore()
+document.body.insertBefore(navBar, document.body.getElementsByTagName('main'))
